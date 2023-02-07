f8c81ed0ec06f6f72f27e60cb640aee8f0e249e8
Paola Andrea García Altamirano <85592476+PaolaAndreaGA@users.noreply.github.com>
2023-02-07 09:44:37 -0500

    Update Percentages_of_Germany.sql

--- Percentages_of_Germany.sql
+++ Percentages_of_Germany.sql
@@ -1,4 +1,4 @@
-###5.
+/* 5.
 Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
 Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
 The format should be Name, Percentage for example:
@@ -6,7 +6,7 @@ name	percentage
 Albania	3%
 Andorra	0%
 Austria	11%
-...	... ###
+...	... ###*/
 
 SELECT name, 
 FORMAT((population/(SELECT population FROM world WHERE name='Germany')),'#0%')
